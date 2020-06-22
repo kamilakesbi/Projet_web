8304576db71cc6056b384764d875d59b241ef19b
felixberge <berge.felix@gmail.com>
2020-06-22 14:41:28 +0200

    Structure MVC

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 $BDD_host="localhost";
 $BDD_user="root";
-$BDD_password=""; // vide sous windows
-$BDD_base=""; // nom de la base de données
+$BDD_password="root"; // vide sous windows
+$BDD_base="web_s6b"; // nom de la base de données
 ?>
