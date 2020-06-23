ae37871ebfb19791daa2839fe6e5dc550c5d0449
ulysse1603 <67162082+ulysse1603@users.noreply.github.com>
2020-06-24 01:23:50 +0200

    Update premier_jet.sql

--- premier_jet.sql
+++ premier_jet.sql
@@ -1,9 +1,4 @@
-
-
-
 /*DROP SCHEMA IF EXISTS JDC ;
-
-
 CREATE SCHEMA JDC;*/
 
  
@@ -12,7 +7,7 @@ CREATE SCHEMA JDC;*/
 -- -------------------------------------------------------------------------- --
 --	Table joueurs
 -- -------------------------------------------------------------------------- --
-drop table JOUEURS cascade;
+
 
 CREATE TABLE JOUEURS
 ( 
@@ -44,12 +39,12 @@ CREATE TABLE JOUEURS
 	CONSTRAINT FK_JOUEURS_SALONS 
 					FOREIGN KEY (salon_actuel) 
 						REFERENCES SALONS(id_salon),
-	constraint ck_JOUEURS_RATIO
+    CONSTRAINT  ck_JOUEURS_RATIO
 					check (ratio between 0 and 1)
-	
-	
-)
+    )
+
 ;
+
 insert into joueurs (mail,pseudo,couleur_pseudo,mdp,connecte,ratio)
 values ('ulysse1603@gmail.com','Ulysse','ROUGE','OK',true,0.89),
 		('KA@gmail.com','Kamilm','Bleu','Ola',true,0.56);
@@ -79,7 +74,6 @@ CREATE TABLE ACTIONS
 	CONSTRAINT FK_ACTIONS_SALONS 
 					FOREIGN KEY (id_salon) 
 						REFERENCES SALONS(id_salon),
-
 	
 	
 	CONSTRAINT CK_ACTIONS_coup_jouee
@@ -88,21 +82,17 @@ CREATE TABLE ACTIONS
 		constraint UK_ACTION unique (id_salon)
 )
 ;
-
-
-
 insert into ACTIONS (id_salon,id_joueur,coup_jouee , date_action ) values
 (5,1,'{{1,0,0,1,0,0,1,0},{1,0,0,1,0,0,1,0},{1,0,0,1,0,0,1,0}}',CURRENT_TIMESTAMP );
 insert into ACTIONS (id_salon,id_joueur,coup_jouee , date_action ) values
 (5,2,'{{1,0,0,1,0,0,1,0},{1,0,0,1,0,0,1,0},{1,0,0,1,0,0,1,0}}',CURRENT_TIMESTAMP );
 insert into ACTIONS (id_salon,id_joueur,coup_jouee , date_action ) values
 (5,1,'{{1,0,0,1,0,0,1,0},{1,0,0,1,0,0,1,0},{1,0,0,1,0,0,1,0}}',CURRENT_TIMESTAMP );
-
 select * from actions a ;*/
 -- -------------------------------------------------------------------------- --
 --	Table salons
 -- -------------------------------------------------------------------------- --
-drop table SALONS cascade;
+drop table if EXISTS SALONS;
  
 CREATE TABLE SALONS 
 ( 
@@ -113,8 +103,8 @@ CREATE TABLE SALONS
 	partie_finie		BOOLEAN not NULL,
 	nom_salon      	varchar(16) NOT NULL, 
 	nb_joueurs   	INTEGER NOT NULL,
-	id_joueurs		INTEGER [4], 
-	nb_carres   	INTEGER [4],
+	id_joueurs		integer [], 
+	nb_carres   	INTEGER [],
 	tour  	integer,
 	
 	vainqueur INTEGER,
@@ -126,8 +116,9 @@ CREATE TABLE SALONS
 	CONSTRAINT PK_SALONS
 					PRIMARY KEY(id_salon),
 
-	constraint CK_SALONS_nb_JOUEUR check (nb_joueurs between 2 and 4),
-	CONSTRAINT CK_SALONS_PARTIE_FINIE check (partie_finie is not null)
+	CONSTRAINT CK_SALONS_nb_JOUEUR check (nb_joueurs between 2 and 4),
+	
+    CONSTRAINT CK_SALONS_PARTIE_FINIE check (partie_finie is not null)
 	
 	
 
@@ -155,5 +146,29 @@ insert into SALONS (partie_commencee,partie_finie,nom_salon ,nb_joueurs,id_joueu
 (true, false , 'test1', 8,'{121,324,659,342,225}','{8,9}',121,'{{1,0,0,1,0,0,1,0},{1,0,1,0,1,0,1,0},{0,1,0,1,1,1,0,1}}' );
 insert into SALONS (partie_commencee,partie_finie,nom_salon ,nb_joueurs,id_joueurs,nb_carres,tour,etat_partie) values 
 (true, false , 'test1', 2,'{121,324,659,342,225}','{8,9}',121,'{{1,0,0,1,0,0,1,0},{1,0,1,0,1,0,1,0},{0,1,0,1,1,1,0,1}}' );
---Mettres les données des tazbleaux entre accolades et les doubler pour tableau double entree
 
+
+-- -------------------------------------------------------------------------- --
+--	Table Messages
+-- -------------------------------------------------------------------------- --
+CREATE TABLE MESSAGES(
+  id_message int(11) NOT NULL COMMENT 'Identifiant du message',
+  id_conversation int(11) NOT NULL COMMENT 'Clé étrangère vers la table des conversations',
+  id_Auteur int(11) NOT NULL COMMENT 'clé étrangère vers la table des auteurs',
+  contenu varchar(100) CHARACTER SET latin1 NOT NULL COMMENT 'Contenu du message'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- -------------------------------------------------------------------------- --
+--	Table Conversations
+-- -------------------------------------------------------------------------- --
+
+CREATE TABLE CONVERSATIONS (
+  id_conv int(11) NOT NULL COMMENT 'Clé primaire',
+  active tinyint(1) NOT NULL DEFAULT '1' COMMENT 'indique si la conversation est active',
+  theme varchar(40) CHARACTER SET latin1 NOT NULL COMMENT 'Thème de la conversation'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+/* Mettres les données des tazbleaux entre accolades et les doubler pour tableau double entree */
