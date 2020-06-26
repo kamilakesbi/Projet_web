0184f0dd12ff7799309952b02a159ae5f87fcc6b
Akesbi Kamil <kam_akesbi23@hotmail.com>
2020-06-26 14:27:35 +0200

    VERSION FINALE

--- vues/profil.php
+++ vues/profil.php
@@ -1,24 +1,6 @@
 <?php
 
-// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-//if (basename($_SERVER["PHP_SELF"]) == "users.php")
-//{
-//	header("Location:../index.php?view=header");
-//	die("");
-//}
 
-
-
-// Hypo : l'user doit etre connecté 
-//if (! valider("connecte","SESSION")) {
-//	header("Location:?view=accueil&msg_feedback=" . urlencode("Il faut etre connecte !"));
-//	die("");
-//}
-
-
-//echo "<h1> Mon profil : <b>$_SESSION[pseudo]</b> : <h1>";
-//echo "Nombre de parties gagnées : <b>$_JOUEURS[nbVictoires]</b> &nbsp; ";
-//echo "Nombre de parties perdues : <b>$_SESSION[nbDefaites]</b> &nbsp; ";
 
 include_once("libs/modele.php");
 include_once("libs/maLibUtils.php"); // tprint
